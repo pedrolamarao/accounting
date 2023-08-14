35ca296a73d3e1ab7579f7ee76fbfee9c1e267ed
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-08-14 18:25:08 -0300

    Upgrde Micronaut plugins to 3.7.10

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     plugins {
         id("com.github.johnrengelman.shadow") version "7.1.2"
-        id("io.micronaut.application") version "3.4.1"
-        id("io.micronaut.library") version "3.4.1"
+        id("io.micronaut.application") version "3.7.10"
+        id("io.micronaut.library") version "3.7.10"
     }
 }
 
